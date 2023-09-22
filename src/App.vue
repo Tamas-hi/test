3c74e653424ca68289daad6d8a9f8f762d919a72
Hargitai Tamas <Tamas.Hargitai@datagroup.de>
2023-09-22 09:57:42 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DeskReservingSystem from './components/DeskReservingSystem.vue'
+</script>
+
+<template>
+  <DeskReservingSystem />
+</template>
+
+<style scoped>
+</style>
